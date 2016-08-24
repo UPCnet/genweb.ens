f7e8279ba885749037d34dcaf10b10af0516b706
Santiago Cortes <santiago.cortes.vaillo@upcnet.es>
2016-08-24 14:43:42 +0200

    Corregir el càlcul dels indicadors
    Els indicadors es calculaven abans de la finalització de la
    transacció i això feia que no fossin precisos per no tenir en
    compte l'impacte de la transacció sobre la base de dades.

--- genweb/ens/indicators/updating.py
+++ genweb/ens/indicators/updating.py
@@ -1,5 +1,8 @@
 import logging
 
+import transaction
+from Products.CMFCore.utils import getToolByName
+
 from genweb.ens.helpers import get_settings_property
 from genweb.core.indicators import RegistryException
 from genweb.core.indicators import WebServiceReporter, ReporterException
@@ -8,11 +11,26 @@ from genweb.ens.indicators.registry import get_registry
 logger = logging.getLogger(name='genweb.ens')
 
 
+def update_if_review_state(content, review_state):
+    workflow_tool = getToolByName(content, 'portal_workflow')
+    if workflow_tool.getInfoFor(content, 'review_state') in review_state:
+        update(context=content)
+
+
 def update(context):
+    transaction.get().addAfterCommitHook(
+        update_after_commit_hook,
+        kws=dict(context=context))
+
+
+def update_after_commit_hook(is_commit_successful, context):
+    if not is_commit_successful:
+        return
     try:
-        registry = get_registry(context)
         ws_url = get_settings_property('ws_endpoint')
         ws_key = get_settings_property('ws_key')
+        registry = get_registry(context)
+
         reporter = WebServiceReporter(ws_url, ws_key)
         reporter.report(registry)
         logger.info("Indicators were successfully reported")
--- genweb/ens/subscribers.py
+++ genweb/ens/subscribers.py
@@ -1,27 +1,18 @@
 from five import grok
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 from Products.CMFCore.interfaces import IActionSucceededEvent
-from Products.CMFCore.utils import getToolByName
 
 from genweb.ens.content.ens import IEns
-from genweb.ens.indicators.updating import update as update_indicators
+from genweb.ens.indicators.updating import (
+    update as update_indicators,
+    update_if_review_state as update_indicators_if_review_state)
 
 
 @grok.subscribe(IEns, IObjectRemovedEvent)
-def update_indicators_on_deletion(obj, event):
-    """
-    Update indicators when a published ens has been deleted.
-    """
-    workflow_tool = getToolByName(obj, 'portal_workflow')
-    if workflow_tool.getInfoFor(obj, 'review_state') in (
-            'intranet', 'published'):
-        update_indicators(context=obj)
+def update_indicators_on_ens_deletion(obj, event):
+    update_indicators_if_review_state(obj, ('intranet', 'published'))
 
 
 @grok.subscribe(IEns, IActionSucceededEvent)
-def update_indicators_on_review_state_change(obj, event):
-    """
-    Update indicators when the ens has been published/unpublished.
-    """
+def update_indicators_on_ens_review_state_change(obj, event):
     update_indicators(context=obj)
-
